ec14308f34f5c7623d45301d524c1e920eaf1c3d
Michael Funk <funkstyr@gmail.com>
2018-06-28 08:08:45 -0700

    move into component structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
